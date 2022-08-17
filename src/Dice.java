23373f26ee4cbf520a211a1e91e5c17396a4f030
Anton T <antoxxa082@ukr.net>
2022-08-17 17:38:41 +0300

    5th task

--- src/Dice.java
+++ src/Dice.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class Dice {
     public static void main(String[] args) {
-        System.out.println("Think of the number from 1 to 6");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Think of the number of sides of the dice");
+        int side = scanner.nextInt();
+        System.out.println("Think of the number from 1 to " +side);
         int num = scanner.nextInt();
-            while (num <= 0 || num >= 7) {
-                System.out.println("The number must be between 1 and 6");
+            while (num <= 0 || num >= side) {
+                System.out.println("The number must be between 1 and " +side);
                 num = scanner.nextInt();
             }
         scanner.close();
         Random random = new Random();
-        int result = random.nextInt(6) + 1;
+        int result = random.nextInt(side) + 1;
         int count = 1;
             while (result != num) {
-                result = random.nextInt(6) + 1;
+                result = random.nextInt(side) + 1;
                 ++count;
             }
             System.out.println("The Dice has been roll " +count +" times");
